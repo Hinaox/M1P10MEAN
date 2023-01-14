f0f9c04d195f15f53e9f9f3189794a4ec3f1cb82
PRGCC Bryan <devpragma@etechconsulting-mg.com>
2023-01-14 20:10:10 +0300

    Connection Base

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # M1P10MEAN
+
+link, username et password projet mongo atlas:
+mongodb+srv://M1P10MEAN:<password>@m1p10mean.b3kaviz.mongodb.net/test
+M1P10MEAN
+mdpsimple
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://M1P10MEAN:mdpsimple@m1p10mean.b3kaviz.mongodb.net/test')
+  .then(() => console.log('Connected!'))
+  .catch(err => console.log(err));
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Coucou World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server started on port ${port}`)
 })
+
