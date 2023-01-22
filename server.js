e1813fa760d12e81dd5f22d1e5456fdf38e2ebfe
PRGCC Bryan <devpragma@etechconsulting-mg.com>
2023-01-22 16:51:28 +0300

    Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose');
+const router = express.Router();
+
+mongoose.set('strictQuery',false);
+mongoose.connect('mongodb+srv://M1P10MEAN:mdpsimple@m1p10mean.b3kaviz.mongodb.net/M1P10MEAN')
+  .then(() => {
+    console.log('Connected!')
+  })
+  .catch(err => console.log('Connection while connecting = '+err))
+
+const app = express()
+const port = 3000
+app.use(cors({
+  origin: 'http://localhost:4200'
+}))
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
