85be227f0f7c44ea64d76aee1b88759c46346c84
PRGCC Bryan <devpragma@etechconsulting-mg.com>
2023-01-24 18:52:28 +0300

    exemple service

--- services/utilisateur.service.js
+++ services/utilisateur.service.js
@@ -1,50 +1,9 @@
 const User = require('../models/utilisateur.model');
-const mongoose = require('mongoose');
-class UserService {
-    // Create user
-    static async create(user) {
-        try {
-            return await User.create(user);
-        } catch (err) {
-            throw err;
-        }
-    }
 
-    // Retrieve all users
-    static async findAll() {
-        try {
-            return await User.find({});
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    // Retrieve user by id
-    static async findOne(id) {
-        try {
-            return await User.findById(id);
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    // Update user by id
-    static async update(id, data) {
-        try {
-            return await User.findByIdAndUpdate(id, data, { new: true });
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    // Delete user by id
-    static async delete(id) {
-        try {
-            return await User.findByIdAndRemove(id);
-        } catch (err) {
-            throw err;
-        }
-    }
+module.export = findUser = async (nom) => {
+    return await User.find({ nom });
 }
 
-module.exports = UserService;
+module.export = find = async () => {
+    return await User.find({},);
+}
