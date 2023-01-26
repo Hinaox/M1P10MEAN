591189e11fd3503ec3615d28c4895214fee19c66
PRGCC Bryan <devpragma@etechconsulting-mg.com>
2023-01-27 00:05:43 +0300

    mail

--- app.js
+++ app.js
@@ -1,36 +1,5 @@
 // const init = require('./db.init.js')
-const server = require('./server.js')
+const server = require('./server.js');
+const sendMail = require('./services/sendMail');
 
-const apiKey = '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';
-
-const axios = require('axios');
-
-const data = {
-  sender: {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-  },
-  to: [
-    {
-      email: 'mahefaran48@gmail.com',
-      name: 'Bryan',
-    },
-  ],
-  subject: 'Test Email',
-  textContent: 'This is a test email sent using the Sendinblue API',
-};
-
-const options = {
-  headers: {
-    'Content-Type': 'application/json',
-    'api-key': apiKey,
-  },
-};
-
-axios.post('https://api.sendinblue.com/v3/smtp/email', data, options)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+sendMail();
