c91f18ee495f6a2f7a72e137833eff516a553f96
Hinaox <Hiaronathanael178@gmail.com>
2023-01-28 00:43:45 +0300

    commit

--- routes/utilisateur.route.js
+++ routes/utilisateur.route.js
@@ -6,11 +6,15 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 // import User service
 const User = require('../models/utilisateur.model');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 // create login route
 router.post('/loginProcess', (req, res) => {
     // find the user by email
     User.findOne({ email: req.body.email }, (err, user) => {
+      console.log(req.body.email);
         if (err) {
             return res.status(500).json({
                 title: "Une erreur s'est produite",
@@ -20,11 +24,11 @@ router.post('/loginProcess', (req, res) => {
         if (!user) {
             return res.status(401).json({
                 title: 'Connexion échouée',
-                message: 'Email ou Mot de passe érroné!' 
+                message: "L'adresse email n'existe pas" 
             });
         }
         // compare the provided password with the hashed password in the database
-        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+      bcrypt.compare(req.body.mdp, user.motDePasse, (err, isMatch) => {
             if (err) {
                 return res.status(500).json({
                     title: "Une erreur s'est produite",
@@ -33,6 +37,7 @@ router.post('/loginProcess', (req, res) => {
                 });
             }
             if (!isMatch) {
+              console.log(isMatch+"test test");
                 return res.status(401).json({
                     title: "Une erreur s'est produite",
                     message: 'Email ou Mot de passe érroné!' 
@@ -40,6 +45,7 @@ router.post('/loginProcess', (req, res) => {
             }
             // create and assign a JSON web token
           const token = jwt.sign({ userId: user._id, role: user.role }, 'secretkey', { expiresIn: 3600*24 });
+          console.log("token:"+token);
           return res.status(200).json({
                 message: 'Successfully logged in',
                 token: token,
@@ -51,7 +57,7 @@ router.post('/loginProcess', (req, res) => {
 
 router.post("/inscriptionProcess", (req, res) => {
   // hash password
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
+  bcrypt.hash(req.password, 10, (err, hash) => {
     if (err) {
       return res.status(500).json({
         title: "Une erreur s'est produite",
@@ -60,10 +66,10 @@ router.post("/inscriptionProcess", (req, res) => {
     }
     // create new user with hashed password
     const newUser = new User({
-      nom: req.body.nom,
-      email: req.body.email,
+      nom: req.nom,
+      email: req.email,
       password: hash,
-      role: req.body.role,
+      role: req.role,
     });
     // save new user
     newUser.save((err, result) => {
@@ -91,8 +97,9 @@ router.get("/nom/:name", (req, res) => {
   });
 });
 
-router.get("/test", (req, res) => {
-  console.log("test fini")
+router.post("/test", (req, res) => {
+  console.log(req.body.mdp);
+  console.log(req.body.email);
   return res.status(200).send("test fini");
 });
 
