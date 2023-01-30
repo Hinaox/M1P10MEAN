2e731832d36431f6342216fe0eae983fdab61c48
Hinaox <Hiaronathanael178@gmail.com>
2023-01-31 00:47:20 +0300

    commit mandeha

---
+++ src/services/checkAuthorization.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const NextFunction = express.NextFunction(); 
+
+// module.exports = checkAuthorization = (req,res,NextFunction) => {
+//   ///traitement authorisation token....
+//     if (authorization === '12345') {
+//     NextFunction();
+//   } else {
+//     res.status(401).send('Tokn invalide');
+//   }
+// }
---
+++ src/services/voiture.service.js
@@ -0,0 +1,7 @@
+// const Voiture = require('../models/voiture.model');
+
+// export const find = async (req,res) => {
+//     return await Voiture.find()
+//       .populate("utilisateur")
+//       .exec();
+// // 
